09bea8be1c71cdcfdd3d1098d6577e7e2807fbdb
Nirupama <tnirupamareddy3@gmail.com>
2024-11-14 22:25:59 +0530

    Update isBalance_ElevateBox.py

--- visaa_prep/isBalance_ElevateBox.py
+++ visaa_prep/isBalance_ElevateBox.py
@@ -1,9 +1,7 @@
 n=int(input())
 arr=list(map(int,input().split()))
-t=[]
 for i in range(n):
     left=sum(arr[0:i])
     right=sum(arr[i+1:])
     diff=abs(left-right)
-    t.append(diff)
-print(*t)
+    print(diff,end=' ')
